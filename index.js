f352a6502406777d075d124cf30a3453de7b2f7e
DefNed <zxcasd555888@gmail.com>
2016-08-02 22:53:14 +0800

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const schedule = require('node-schedule');
 const touPiaoNum = 2;
 const url = "http://s1.eqxiu.com/eqs/scene/counter/values?sceneId=52759457&fieldIds=55629006292%2C352544811%2C55628455361%2C55616572961%2C55623913260%2C556233132107%2C9107582991%2C55616796725%2C2566006331%2C55625931086%2C7757361180%2C55624260874%2C55617485510%2C55626501562%2C1981355588%2C55628276973%2C55626119240%2C55628672160%2C55629191326%2C55629612679%2C55616286071%2C9048866244%2C761369311%2C55626353614%2C55662201577%2C55629782674";
-// request(url, requestFunc);
+
 var j = schedule.scheduleJob('1 * * * * *', function () {
     console.log('定时任务开始!');
     request(url, requestFunc);
